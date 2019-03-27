0f0eb71ec2c37dfabd78eda49b2eb58b5cfd4227
Minh <minhdotcom@gmail.com>
2019-03-27 15:51:59 +0700

    xong

---
+++ myFunction.js
@@ -0,0 +1,8 @@
+let inputwidth;
+let inputheight;
+inputwidth = prompt('nhap chieu rong');
+inputheight = prompt('nhap chieu dai');
+let width = parseInt(inputwidth);
+let height = parseInt(inputheight);
+let area = width * height;
+document.write("dien tich la " + area);
